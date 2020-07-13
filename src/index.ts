57bcf320f308c192c9d61cd71715d0e4c398e376
Tobias Schindler <t.schindler@gambio.de>
2020-07-13 23:48:50 +0200

    alot improvements and some cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
-import ExampleApp, {
-  ExampleAppRenderDescriptor,
-} from './WebGl/Examples/GlTfMesh/index';
 import createSurface from './WebGl/Util/createSurface';
 import { mat4, vec3 } from 'gl-matrix';
 import resizeCanvas from './WebGl/Util/resizeCanvas';
+import Mesh, { MeshRenderDescriptor } from './WebGl/Mesh/Mesh';
 
 import './styles/main.scss';
 
 const surface = createSurface('surface');
 
-ExampleApp.init(surface.gl).then((app) => {
+Mesh.init({ gl: surface.gl, url: './3DModels/Suzanne/glTf/monkey_sample.glb' }).then((mesh: Mesh) => {
   const projectionMatrix = mat4.create();
   const aspect = Math.abs(surface.canvas.width / surface.canvas.height);
   const degree = 60;
@@ -20,7 +18,7 @@ ExampleApp.init(surface.gl).then((app) => {
   const viewMatrix = mat4.create();
   mat4.translate(viewMatrix, viewMatrix, vec3.fromValues(0, -3, -10));
 
-  const descriptor: ExampleAppRenderDescriptor = {
+  const descriptor: MeshRenderDescriptor = {
     gl: surface.gl,
     matrices: {
       projection: projectionMatrix,
@@ -29,13 +27,13 @@ ExampleApp.init(surface.gl).then((app) => {
     light: {
       color: vec3.fromValues(0.8, 0.8, 0.8),
       // color: vec3.fromValues(1.0, 1.0, 1.0),
-      position: vec3.fromValues(0.0, 0.0, 2.0)
-    }
+      position: vec3.fromValues(0.0, 0.0, 2.0),
+    },
   };
   const gl = surface.gl;
 
   const renderLoop = () => {
-    app.rotateX(0.02);
+    mesh.rotateY(0.05);
 
     gl.clearColor(0.1, 0.1, 0.1, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -46,7 +44,7 @@ ExampleApp.init(surface.gl).then((app) => {
     gl.enable(gl.CULL_FACE);
     gl.enable(gl.DEPTH_TEST);
 
-    app.render(descriptor);
+    mesh.render(descriptor);
     requestAnimationFrame(renderLoop);
   };
   renderLoop();
